1d33ac1646f0439d515325a3d72d1b21cc1ca3d6
cnoss <christian.noss@th-koeln.de>
2022-07-01 11:38:16 +0200

    Mittelschöne Implementierung mit Hilfe einer JS Klasse

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,34 @@
+/* Classes 
+############################################################################ */
+class NavInteraction {
+  constructor(ele){
+    this.navBar = ele;
+    this.navLinks = this.navBar.querySelectorAll('a');
+    this.lastActiveNavElement = false;
+  }
+
+  addInteraction() {
+    this.navLinks.forEach(item => {
+      const navItem = item.closest('li');
+      navItem.addEventListener('click', (ele) => {
+        const { currentTarget } = ele;
+        this.changeStateOfNavItem(currentTarget);
+      });
+    });
+  }
+
+  changeStateOfNavItem(target){
+    if(this.lastActiveNavElement) this.lastActiveNavElement.dataset.state = '';
+    target.dataset.state = 'active';
+    this.lastActiveNavElement = target;
+  }
+
+}
+
 /* Functions 
 ############################################################################ */
-const navInteraction = () => {
-  const navbar = document.querySelector('[data-js-navbar]');
-  const navLinks = navbar.querySelectorAll('a');
-    
-  navLinks.forEach(item => {
-    const navItem = item.closest('li');
-    navItem.addEventListener('click', (ele) => {
-      const { currentTarget } = ele;
-      changeStateOfNavItem(currentTarget);
-    });
-  });
-}
 
-let lastActiveNavElement = false;
-
-const changeStateOfNavItem = (target) => {
-  if(lastActiveNavElement) lastActiveNavElement.dataset.state = '';
-  target.dataset.state = 'active';
-  lastActiveNavElement = target;
-}
-
-const scrollSpy = () => {
+const scrollSpy = (navInteraction) => {
   const intersectionObserver = new IntersectionObserver(function(entries) {
     if (entries[0].intersectionRatio <= 0) return;
     const { target } = entries[0];
@@ -29,18 +36,21 @@ const scrollSpy = () => {
     
     const navLink = document.querySelector(`a[href*=${id}]`);
     const navItem = navLink.closest('li');
-    changeStateOfNavItem(navItem);
+    navInteraction.changeStateOfNavItem(navItem);
   });
 
   const elementsWithId = document.querySelectorAll('main > [id]');
   elementsWithId.forEach( ele => intersectionObserver.observe(ele));
 }
 
-
 /* Main 
 ############################################################################ */
 
 document.addEventListener('DOMContentLoaded', () => {
-  navInteraction();
-  scrollSpy();
+  // navInteraction();
+  const navbar = document.querySelector('.navbar');
+  const navInteraction = new NavInteraction(navbar);
+  navInteraction.addInteraction();
+  
+  scrollSpy(navInteraction);
 });
