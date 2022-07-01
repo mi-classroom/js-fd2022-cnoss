e3a4032a96a03dec5771aaae5a2e5308dc5c6de4
cnoss <christian.noss@th-koeln.de>
2022-07-01 13:07:54 +0200

    Kommentare via JSON addiert

--- scripts/main.js
+++ scripts/main.js
@@ -43,6 +43,29 @@ const scrollSpy = (navInteraction) => {
   elementsWithId.forEach( ele => intersectionObserver.observe(ele));
 }
 
+const fetchComments = () => {
+  fetch('/json/comments.json')
+  .then(response => response.json())
+  .then(data => renderComments(data));
+}
+
+const renderComments = (comments) => {
+  const target = document.querySelector('[data-js-comments]')
+  const commentList = comments.map(comment => {
+    return `
+      <figure class="comment">
+        <img class="comment_avatar" src="/images/avatars/${comment.avatar}" alt="${comment.firstname} ${comment.lastname}">
+        <figcaption>
+          <h3 class="comment__name">${comment.firstname} ${comment.lastname}</h3>
+          <p class="comment__text">${comment.comment}</p>
+          <date class="comment__date">${comment.date}</date>
+        </figcaption>
+      </figure>
+    `;
+  });
+
+  target.innerHTML = commentList.join("");
+}
 /* Main 
 ############################################################################ */
 
@@ -53,4 +76,5 @@ document.addEventListener('DOMContentLoaded', () => {
   navInteraction.addInteraction();
   
   scrollSpy(navInteraction);
+  fetchComments();
 });
