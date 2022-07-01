b331e83ca231c233ce7a9d5ca58d75d8a9771ef7
cnoss <christian.noss@th-koeln.de>
2022-07-01 11:27:49 +0200

    Scrollspy integriert

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,46 @@
-document.addEventListener('DOMContentLoaded', () => {
-  
+/* Functions 
+############################################################################ */
+const navInteraction = () => {
   const navbar = document.querySelector('[data-js-navbar]');
   const navLinks = navbar.querySelectorAll('a');
-  let lastActiveNavElement = false;
     
   navLinks.forEach(item => {
     const navItem = item.closest('li');
     navItem.addEventListener('click', (ele) => {
       const { currentTarget } = ele;
-      if(lastActiveNavElement) lastActiveNavElement.dataset.state = '';
-      currentTarget.dataset.state = 'active';
-      lastActiveNavElement = currentTarget;
+      changeStateOfNavItem(currentTarget);
     });
+  });
+}
+
+let lastActiveNavElement = false;
+
+const changeStateOfNavItem = (target) => {
+  if(lastActiveNavElement) lastActiveNavElement.dataset.state = '';
+  target.dataset.state = 'active';
+  lastActiveNavElement = target;
+}
+
+const scrollSpy = () => {
+  const intersectionObserver = new IntersectionObserver(function(entries) {
+    if (entries[0].intersectionRatio <= 0) return;
+    const { target } = entries[0];
+    const { id } = target;
     
+    const navLink = document.querySelector(`a[href*=${id}]`);
+    const navItem = navLink.closest('li');
+    changeStateOfNavItem(navItem);
   });
 
+  const elementsWithId = document.querySelectorAll('main > [id]');
+  elementsWithId.forEach( ele => intersectionObserver.observe(ele));
+}
+
+
+/* Main 
+############################################################################ */
+
+document.addEventListener('DOMContentLoaded', () => {
+  navInteraction();
+  scrollSpy();
 });
